c3bb5c0eea90a1a82167b2ee5600923b70560e09
createoop <dcmntik@gmail.com>
2018-07-27 02:40:55 +0300

    CoinmateApi - First commit

---
+++ src/Coinmate.php
@@ -0,0 +1,78 @@
+<?php namespace CoinmateApi;
+
+class Coinmate
+{
+    private $requestor = null;
+
+    private $publicKey = null;
+
+    private $privateKey = null;
+
+    private $clientId = null;
+
+    private $url = 'https://coinmate.io/api';
+
+    private $nonce = null;
+
+    private $signature = null;
+
+    function __construct($publicKey = null, $privateKey = null, $clientId = null)
+    {
+        $this->requestor = new Requestor();
+        $this->publicKey = $publicKey;
+        $this->privateKey = $privateKey;
+        $this->clientId = $clientId;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    public function setCredentials($publicKey, $privateKey, $clientId)
+    {
+        $this->publicKey = $publicKey;
+        $this->privateKey = $privateKey;
+        $this->clientId = $clientId;
+    }
+
+    private function  generateSignature()
+    {
+        // Generate unique number
+        $this->nonce = str_replace('.', '', microtime(true));
+
+        // Generate request signature
+        $tmp = $this->nonce.$this->clientId.$this->publicKey;
+        $tmp = hash_hmac('sha256', $tmp, $this->privateKey);
+
+        // Set signature
+        $this->signature = strtoupper($tmp);
+    }
+
+    private function splitWithCredentials(array $request)
+    {
+        $request['clientId'] = $this->clientId;
+        $request['publicKey'] = $this->publicKey;
+        $request['nonce'] = $this->nonce;
+
+        $this->generateSignature();
+
+        $request['signature'] = $this->signature;
+
+        return $request;
+    }
+
+    public function getTicker($from = null, $to = null)
+    {
+        return $this->requestor->sendRequest("{$this->url}/ticker", "get", [
+            'currencyPair' => "{$from}_{$to}"
+        ]);
+    }
+
+    public function transactions($minutes = 10)
+    {
+        return $this->requestor->sendRequest("{$this->url}/transactions", "get", [
+            'minutesIntoHistory' => $minutes
+        ]);
+    }
+}
---
+++ src/Requestor.php
@@ -0,0 +1,46 @@
+<?php namespace CoinmateApi;
+
+class Requestor
+{
+
+    public function sendRequest($path, $method, array $request)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        switch ($method)
+        {
+            case "post":
+                curl_setopt($ch, CURLOPT_URL, $path);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($request));
+            break;
+
+            case "get":
+                curl_setopt($ch, CURLOPT_URL, $path."?".http_build_query($request));
+            break;
+        }
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/x-www-form-urlencoded"]);
+
+        $response = curl_exec($ch);
+        $error = curl_error($ch);
+        $errorCode = curl_errno($ch);
+
+        curl_close($ch);
+
+        if ( ! $error) {
+            $result = json_decode($response, 1);
+        } else {
+            $result['curl_err_code'] = $errorCode;
+            $result['curl_err_msg'] = $error;
+        }
+
+        return $result;
+    }
+
+}
+
